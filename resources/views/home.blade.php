ff3bad5ec1f55e843983638e2e4f5eaa9c370ff9
Narciso Batingal <batingalnarz11@gmail.com>
2018-06-29 13:08:02 +0800

    updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Records</div>
+                <div class="panel-heading">Records List</div>
 
                 <div class="panel-body"><table class="table">
                   <thead>
